71add84aab097f5531f520c581c24bcbb70d2123
Silvia <ue246422@edu.xunta.gal>
2021-03-03 01:24:46 +0100

    CirculoTest cambios y documentación

--- test/circulo/CirculoTest.java
+++ test/circulo/CirculoTest.java
@@ -1,141 +1,145 @@
 package circulo;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Pruebas para la clase Circulo
- * @author profesor
+ * Pruebas Unitarias para la clase Circulito.
+ *
+ * @author profesor, Silvia López Diéguez
+ * @version 03/03/2021
+ * @see src.Circulito
  */
 public class CirculoTest {
 
     /**
-     * Test of establecerX method, of class Circulo.
+     * Test del método establecerX, de la clase Circulito.
      */
     @Test
     public void testEstablecerX() {
         System.out.println("establecerX");
         int valorX = 0;
-        Circulo instance = new Circulo();
-        instance.establecerX(valorX);
+        Circulito instance = new Circulito();
+        instance.setCoordenadaX(valorX);
     }
 
     /**
-     * Test of establecerY method, of class Circulo.
+     * Test del método establecerY, de la clase Circulito.
      */
     @Test
     public void testEstablecerY() {
         System.out.println("establecerY");
         int valorY = 0;
-        Circulo instance = new Circulo();
-        instance.establecerY(valorY);
+        Circulito instance = new Circulito();
+        instance.setCoordenadaY(valorY);
     }
 
     /**
-     * Test of establecerRadio method, of class Circulo.
+     * Test del método establecerRadio, de la clase Circulito.
      */
     @Test
     public void testEstablecerRadio() {
         System.out.println("establecerRadio");
-        Circulo instance = new Circulo();
-        instance.establecerRadio(0.0);
-        instance.establecerRadio(0.1);
-        instance.establecerRadio(-0.1);
+        Circulito instance = new Circulito();
+        instance.setRadio((0.0 < Circulito.getLimiteradio() ? 0.0 : 0.0));
+        instance.setRadio((0.1 < Circulito.getLimiteradio() ? 0.0 : 0.1));
+        double valorRadio = -0.1;
+        instance.setRadio((valorRadio < Circulito.getLimiteradio() ? 0.0 : valorRadio));
     }
 
     /**
-     * Test of obtenerX method, of class Circulo.
+     * Test del método obtenerX, de la clase Circulit.
      */
     @Test
     public void testObtenerX() {
         System.out.println("obtenerX");
-        Circulo instance = new Circulo(0,0,0.1);
+        Circulito instance = new Circulito(0, 0, 0.1);
         int expResult = 0;
-        int result = instance.obtenerX();
+        int result = instance.getCoordenadaX();
         assertEquals(expResult, result);
     }
 
-
     /**
-     * Test of obtenerY method, of class Circulo.
+     * Test del método obtenerY, de la clase Circulito.
      */
     @Test
     public void testObtenerY() {
         System.out.println("obtenerY");
-        Circulo instance = new Circulo(0,0,0.1);
+        Circulito instance = new Circulito(0, 0, 0.1);
         int expResult = 0;
-        int result = instance.obtenerY();
+        int result = instance.getCoordenadaY();
         assertEquals(expResult, result);
     }
 
     /**
-     * Test of obtenerRadio method, of class Circulo.
+     * Test del método obtenerRadio, de la clase Circulito.
      */
     @Test
     public void testObtenerRadio() {
         System.out.println("obtenerRadio");
-        Circulo instance = new Circulo(0,0,0.1);
-        double result = instance.obtenerRadio();
-        assertEquals(0.1, result,0);
+        Circulito instance = new Circulito(0, 0, 0.1);
+        double result = instance.getRadio();
+        assertEquals(0.1, result, 0);
 
-        instance.establecerRadio(0.1);
-        result= instance.obtenerRadio();
-        assertEquals(0.1, result,0);
+        instance.setRadio((0.1 < Circulito.getLimiteradio() ? 0.0 : 0.1));
+        result = instance.getRadio();
+        assertEquals(0.1, result, 0);
+        double valorRadio = -0.1;
 
-        instance.establecerRadio(-0.1);
-        result= instance.obtenerRadio();
-        assertEquals(0.0, result,0);
+        instance.setRadio((valorRadio < Circulito.getLimiteradio() ? 0.0 : valorRadio));
+        result = instance.getRadio();
+        assertEquals(0.0, result, 0);
     }
 
     /**
-     * Test of obtenerDiametro method, of class Circulo.
+     * Test del método obtenerDiametro, de la clase Circulito.
      */
     @Test
     public void testObtenerDiametro() {
         System.out.println("obtenerDiametro");
-        Circulo instance = new Circulo(0,0,0.1);
+        Circulito instance = new Circulito(0, 0, 0.1);
         double result = instance.obtenerDiametro();
-        assertEquals(0.2, result,0);
+        assertEquals(0.2, result, 0);
     }
 
     /**
-     * Test of obtenerCircunferencia method, of class Circulo.
+     * Test del método obtenerCircunferencia, de la clase Circulito.
      */
     @Test
     public void testObtenerCircunferencia() {
         System.out.println("obtenerCircunferencia");
-        Circulo instance = new Circulo(0,0,0.1);
+        Circulito instance = new Circulito(0, 0, 0.1);
         double expResult = 0.6283185;
         double result = instance.obtenerCircunferencia();
-        assertEquals(expResult, result,1e-6);
+        assertEquals(expResult, result, 1e-6);
     }
 
     /**
-     * Test of obtenerArea method, of class Circulo.
+     * Test del método obtenerArea, de la clase Circulito.
      */
     @Test
     public void testObtenerArea() {
         System.out.println("obtenerArea");
-        Circulo instance = new Circulo(0,0,0.1);
+        Circulito instance = new Circulito(0, 0, 0.1);
         double expResult = 0.0314159;
-        double result = instance.obtenerArea();
-        assertEquals(expResult, result,1e-6);
+        double result = instance.obtenerAreaCirculo();
+        assertEquals(expResult, result, 1e-6);
     }
 
-    
     /**
-     * Test of trasladarCentro method, of class Circulo.
+     * Test del método trasladarCentro, de la clase Circulito.
      */
     @Test
     public void testTrasladarCentro() {
         System.out.println("trasladarCentro");
-        Circulo instance = new Circulo();
-        int resultx=instance.obtenerX();
-        int resulty=instance.obtenerY();
+        Circulito instance = new Circulito();
+        int resultx = instance.getCoordenadaX();
+        int resulty = instance.getCoordenadaY();
         instance.trasladarCentro();
-        int resultnx = instance.obtenerX();
-        int resultny = instance.obtenerY();
-        assertEquals(resultx+5, resultnx);
-        assertEquals(resulty+6, resultny);
+        int resultnx = instance.getCoordenadaX();
+        int resultny = instance.getCoordenadaY();
+        assertEquals(resultx + 5, resultnx);
+        assertEquals(resulty + 6, resultny);
     }
 
 }
